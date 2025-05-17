944e7f4e695c5f9c46b47313aaf872c5149d249d
02V225 <korobowladimir@yandex.ru>
2025-05-17 17:18:49 +0300

    Изменённый код главного героя
    Я уже устал писать описание

--- MainCharacterMovement.gd
+++ MainCharacterMovement.gd
@@ -1,49 +1,34 @@
 extends CharacterBody2D
 
-const acceleration = 200.0
+
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -450.0
 var HEAVE = 100.0
 
+
 func _physics_process(delta: float) -> void:
+	$ProgressBar.value = HEAVE
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
-		if collision.get_collider().name == "CharacterBody2D2":
-			HEAVE -= 20
-			print("Здоровье: ", HEAVE)
+		if collision.get_collider() is CharacterBody2D :
+			HEAVE -= 5
+			print("Здоровье : ", HEAVE)
 			if HEAVE <= 0:
 				queue_free()
-	
-	var direction = Vector2.ZERO
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-		$MainCharacterAnimation.flip_h= true
-		$MainCharacterAnimation.play()
-	if Input.is_action_pressed("move_rigth"):
-		$MainCharacterAnimation.flip_h= false
-		$MainCharacterAnimation.play()
-		direction.x += 1
-	if Input.is_action_pressed("move_up"):
-		$MainCharacterAnimation.play()
-		direction.y -= 1
-	if Input.is_action_pressed("move_down"):
-		$MainCharacterAnimation.play()
-		direction.y += 1
-	if Input.is_action_just_released("move_left"):
-		$MainCharacterAnimation.stop()
-	if Input.is_action_just_released("move_rigth"):
-		$MainCharacterAnimation.stop()
-	if Input.is_action_just_released("move_up"):
-		$MainCharacterAnimation.stop()
-	if Input.is_action_just_released("move_down"):
-		$MainCharacterAnimation.stop()
 	# Add the gravity.
-	if direction.length() > 0:
-		direction = direction.normalized()
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	velocity = direction * SPEED
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
