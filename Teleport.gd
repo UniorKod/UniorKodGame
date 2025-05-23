39d3dc746f428a08191585b6d32979e6a19cf8c1
Artem-haus <artemknazan228@gmail.com>
2025-05-23 14:34:28 +0300

    указал что можно перемещаться между сценами
    Добавил два скрипта с перемещением персонажа между сценами и настроил 3 сцены

---
+++ Teleport.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready():
+	# Подключаем сигнал через Callable (Godot 4.x)
+	body_entered.connect(_on_body_entered)  # <- передаём функцию, а не строку!
+
+func _on_body_entered(body: Node):
+	if body.name == "Главный":
+		get_tree().change_scene_to_file("res://Game2Level.tscn")
