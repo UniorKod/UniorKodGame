c352734d59a68a45ddae02bbf762470640ca2e76
Yaroslav55501 <yaroslavledovskiy05@gmail.com>
2025-05-24 17:15:01 +0300

    Настроил скрипт смерти
    При смерти переходим на сцену смерти где можно перейти обратно

--- MainCharacterMovement.gd
+++ MainCharacterMovement.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -570.0
 var HEAVE = 100.0
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -14,6 +15,7 @@ func _physics_process(delta: float) -> void:
 			HEAVE -= 5
 			print("Здоровье : ", HEAVE)
 			if HEAVE <= 0:
+				get_tree().change_scene_to_file("res://Сцена смерти.tscn")
 				queue_free()
 	# Add the gravity.
 	if not is_on_floor():
@@ -22,6 +24,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
